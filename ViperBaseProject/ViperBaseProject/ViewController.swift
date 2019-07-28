81985e3fc403adf86b59afd2c3bca69951901528
Mate Lorincz <mate.lorincz@cellum.com>
2019-07-28 13:59:05 +0300

    Create empty Xcode project.

---
+++ ViperBaseProject/ViperBaseProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperBaseProject
+//
+//  Created by Mate Lorincz on 2019. 07. 28..
+//  Copyright © 2019. MateLorincz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
