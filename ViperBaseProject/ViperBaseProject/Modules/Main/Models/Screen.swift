d8c42036734f683ae729d1fee8a00db92454871a
Mate Lorincz <matt.lorincz@gmail.com>
2019-07-29 18:49:30 +0300

    Add missing header comments.

--- ViperBaseProject/ViperBaseProject/Modules/Main/Models/Screen.swift
+++ ViperBaseProject/ViperBaseProject/Modules/Main/Models/Screen.swift
@@ -1,3 +1,10 @@
+//
+//  Screen.swift
+//  ViperBaseProject
+//
+//  Created by Mate Lorincz on 2019. 07. 28..
+//  Copyright © 2019. MateLorincz. All rights reserved.
+//
 
 import Foundation
 
--- ViperBaseProject/ViperBaseProject/Protocols/Wireframe.swift
+++ ViperBaseProject/ViperBaseProject/Protocols/Wireframe.swift
@@ -1,3 +1,10 @@
+//
+//  Wireframe.swift
+//  ViperBaseProject
+//
+//  Created by Mate Lorincz on 2019. 07. 28..
+//  Copyright © 2019. MateLorincz. All rights reserved.
+//
 
 import UIKit
 
